becba5bc8a7d90fecec63fe3b039c4f0dc6b4574
q4c-tsuchiya <tsuchiya@q4c.jp>
2019-04-29 11:47:55 +0900

    sample

--- frontend/project/store/index.js
+++ frontend/project/store/index.js
@@ -3,6 +3,14 @@ export const state = () => ({
   counter: 0
 })
 
+export const actions = {
+  async sample({ commit }) {
+    // API通信
+    // const response = await apiGetSomething()
+    // commit('sample', response)
+  }
+}
+
 export const mutations = {
   increment(state) {
     state.counter++
